2d749d8f329074b1cb2dff21802ef3d14f0a3981
PoppyCurtis <poppy.curtis@gmail.com>
2019-03-12 17:00:58 +0000

    Use res.write function to display two messages to client side - date and price comparison

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ app.post("/", function(req, res) {
     request(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${crypto}${fiat}`, function(error, response, body) {
         var data = JSON.parse(body);
         var price = data.last;
-        res.send(`<h1>The current price of ${crypto} is ` + price + ` ${fiat}</h1>`)
+        var currentDate = data.display_timestamp;
+        res.write(`<p>The current date is ${currentDate}</p>`);
+        res.write(`<h1>The current price of ${crypto} is ` + price + ` ${fiat}</h1>`);
+        res.send()
     });
 });
 
